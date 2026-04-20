3d226cac245e81efea30f5285efa55c2817f9dab
Yasasmie Rajapakshe <wruyasas0217@gmail.com>
2026-04-20 13:55:02 +0530

    Added Room model and in-memory DataStore
    Created Room entity with attributes and sensor tracking.
    Implemented in-memory DataStore using HashMap to simulate database storage.

---
+++ smart-campus-api/src/main/java/com/smartcampus/store/DataStore.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.store;
+
+import com.smartcampus.model.Room;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DataStore {
+    public static Map<String, Room> rooms = new HashMap<>();
+}
