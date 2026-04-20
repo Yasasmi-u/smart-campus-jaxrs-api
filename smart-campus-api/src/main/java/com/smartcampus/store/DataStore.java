89ba3df43aece67e9eccd809feba4c4cfcf9388e
Yasasmie Rajapakshe <wruyasas0217@gmail.com>
2026-04-20 15:21:10 +0530

    Added SensorReading model and readings storage to DataStore
    Created SensorReading entity and extended DataStore to support in-memory storage of sensor reading history.

--- smart-campus-api/src/main/java/com/smartcampus/store/DataStore.java
+++ smart-campus-api/src/main/java/com/smartcampus/store/DataStore.java
@@ -5,8 +5,11 @@
 package com.smartcampus.store;
 
 import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,4 +18,6 @@ import java.util.Map;
  */
 public class DataStore {
     public static Map<String, Room> rooms = new HashMap<>();
+    public static Map<String, Sensor> sensors = new HashMap<>();
+    public static Map<String, List<SensorReading>> readings = new HashMap<>();    
 }
